63ea523c69fb17a2d60a234ae0489b8d1339dc7d
ALEXANDRA PASTOR <pastoralex@free.fr>
2025-06-13 16:43:55 +0200

    Update main.tsx
    Delete bootstrap

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 // Import the main app component
 import App from "./App";
