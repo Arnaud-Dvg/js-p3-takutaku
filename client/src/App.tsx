aa00abb4b37dbc21381db04a0fe5bfdf38697334
thibaud.guadagna <thibaud.guadagna@gmail.com>
2025-06-16 17:35:08 +0200

    Add Responsive Header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { Outlet } from "react-router-dom";
 
 import { AnimeProvider } from "../context/AnimeContext";
+import Header from "./components/Header/DesktopNavBar";
 
 function App() {
   return (
     <>
       <div>
         <AnimeProvider>
+          <Header />
           <Outlet />
         </AnimeProvider>
       </div>
---
+++ client/src/components/Mobile Header/BurgerButton.tsx
@@ -0,0 +1,33 @@
+type BurgerProps = {
+  toggleMenu: () => void;
+  isOpen: boolean;
+};
+
+function Burger({ toggleMenu, isOpen }: BurgerProps) {
+  return (
+    <section className="z-50  rounded-sm h-9 w-10 cursor-pointer p-1">
+      <div onClick={toggleMenu} onKeyDown={toggleMenu}>
+        <div className="relative w-11 h-11">
+          <span
+            className={`absolute left-0 h-0.5 w-8 bg-white transform transition-all duration-300 ease-in-out ${
+              isOpen ? "rotate-45 top-2.5" : "top-0"
+            }`}
+          />
+
+          <span
+            className={`absolute left-0 h-0.5 w-8 bg-white transition-all duration-300 ease-in-out ${
+              isOpen ? "opacity-0" : "top-2.5"
+            }`}
+          />
+          <span
+            className={`absolute left-0 h-0.5 w-8 bg-white transform transition-all duration-300 ease-in-out ${
+              isOpen ? "-rotate-45 top-2.5" : "top-5"
+            }`}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Burger;
